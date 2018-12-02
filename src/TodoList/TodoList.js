531b3a3b4636caa616679eb0bda8abded6fd8cbe
jmstevens <jmstevens@meditech.com>
2018-12-02 15:32:46 -0500

    Adjusted delete button and  item titles

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -5,15 +5,16 @@ import './TodoList.css';
 const todolist = props => {
   const checkboxClassName = props.checked ? 'todo-checkbox-text is-checked' : 'todo-checkbox-text';
   const delTitle = 'Delete ' + props.item;
+  const doneTitle = 'Mark ' + props.item +' done';
 
   return (
     <div className='todo-card'>
       <div className='todo-delete-container'>
-        <span onClick={props.click} className='todo-delete' title={delTitle}>X</span>
+        <button onClick={props.click} className='todo-delete' title={delTitle}>X</button>
       </div>
       <div>
-        <input type='checkbox' title='Mark done' className='todo-checkbox' id={props.id} onChange={props.check} />
-        <label title='Mark done' htmlFor={props.id} className={checkboxClassName}>{props.item}</label>
+        <input type='checkbox' title={doneTitle} className='todo-checkbox' id={props.id} onChange={props.check} />
+        <label title={doneTitle} htmlFor={props.id} className={checkboxClassName}>{props.item}</label>
       </div>
     </div>
   );
